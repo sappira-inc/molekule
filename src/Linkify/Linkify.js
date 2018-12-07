297520f89681f66357ccdb69b6a1955cc7ebd46e
Kyle Alwyn <kjalwyn@gmail.com>
2018-12-07 11:35:39 -0800

    Improve link parsing; Update docs to showcase more complex functionality

--- src/Linkify/Linkify.js
+++ src/Linkify/Linkify.js
@@ -2,45 +2,48 @@ import React from 'react';
 
 const URL_REGEX = /(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#/%?=~_|!:,.;]*[-A-Z0-9+&@#/%=~_|])/gi;
 
-const detectLinks = ({ children, linkStyle = {} }) =>
+const Link = ({ href, linkStyle }) => (
+  <a
+    target="_blank"
+    rel="noopener noreferrer"
+    style={{
+      color: 'inherit',
+      textDecoration: 'underline',
+      ...linkStyle,
+    }}
+    href={href}>
+    {href}
+  </a>
+);
+
+const linkify = ({ children, linkStyle = {} }) =>
   React.Children.map(children, child => {
     if (!child) return null;
 
     if (typeof child === 'string') {
-      const words = child.split(' ');
-      return words.map((word, index) => {
-        const isLastWord = words.length - 1 === index;
+      const matches = child.match(URL_REGEX);
 
-        if (URL_REGEX.test(word)) {
-          return (
-            <>
-              <a
-                key={word}
-                target="_blank"
-                rel="noopener noreferrer"
-                style={{
-                  color: 'inherit',
-                  textDecoration: 'underline',
-                  ...linkStyle,
-                }}
-                href={word}>
-                {word}
-              </a>
+      if (!matches) {
+        return child;
+      }
 
-              {isLastWord ? '' : ' '}
-            </>
-          );
-        }
+      // Clone the string for manipulation
+      let string = `${child}`;
 
-        return isLastWord ? word : `${word} `;
-      });
+      // Run through the url matches to create our array of string parts and Link components
+      const stringParts = matches.reduce((parts, url) => {
+        const urlStartIndex = string.indexOf(url);
+        const partBeforeUrl = string.substring(0, urlStartIndex);
+        parts.push(partBeforeUrl, <Link href={url} linkStyle={linkStyle} />);
+        string = string.substring(urlStartIndex + url.length);
+        return parts;
+      }, []);
+
+      // Add any leftovers after the last url
+      return [...stringParts, string].filter(Boolean);
     }
 
-    if (React.isValidElement(child)) {
-      return detectLinks({ children: child.props.children, linkStyle });
-    }
-
-    return child;
+    return linkify({ children: child.props.children, linkStyle });
   });
 
-export default props => detectLinks(props);
+export default props => linkify(props);
