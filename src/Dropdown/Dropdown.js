c7a5d889b9cd4573a45dbd7df060b09462ad1451
cehsu <claire@sappira.com>
2019-03-04 11:04:07 -0800

    Add comments about focus/blur timer

--- src/Dropdown/Dropdown.js
+++ src/Dropdown/Dropdown.js
@@ -127,10 +127,16 @@ export default class Dropdown extends React.Component {
     }
   }
  
+
+  // We close the popover on the next tick by using setTimeout.
+  // This is necessary because we need to first check if
+  // another child of the element has received focus as
+  // the blur event fires prior to the new focus event.
   handleBlur = () => {
     this.timer = setTimeout(this.autoclose);
   }
  
+  // If a child receives focus, do not close the popover.
   handleFocus = () => { 
     clearTimeout(this.timer);
   }
