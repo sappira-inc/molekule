fc625751f5d19caa2ba448d727988a8522bcf0b1
Kyle Alwyn <kjalwyn@gmail.com>
2019-03-22 16:33:08 -0700

    Handle case where passed value changes

--- src/Form/PhoneInput.js
+++ src/Form/PhoneInput.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { AsYouType } from 'libphonenumber-js';
 import Input from './Input';
@@ -6,18 +6,28 @@ import { createEasyInput } from './EasyInput';
 
 function PhoneInput({ value: passedValue, onChange, countryCode, ...inputProps }) {
   const formatter = new AsYouType(countryCode);
-  const formatPhoneNumber = val => {
-    const output = formatter.input(val);
+
+  const formatPhoneNumber = (newVal, prevVal = '') => {
+    const isDeletingParen = prevVal && prevVal.substr(-1) === ')' && newVal.length < prevVal.length;
+    const output = isDeletingParen ? newVal : formatter.input(newVal);
     formatter.reset();
     return output;
   };
+
   const [value, setValue] = useState(formatPhoneNumber(passedValue));
 
-  const handleInputChange = (name, val) => {
-    const output = value && value.substr(-1) === ')' && val.length < value.length ? val : formatPhoneNumber(val);
-    setValue(output);
-    onChange(name, val);
+  const handleInputChange = (name, newVal) => {
+    // Formatter adds a paren to "(408" so we skip formatting if current value has the paren
+    const formatted = formatPhoneNumber(newVal, value);
+    setValue(formatted);
+    onChange(name, newVal);
   };
+
+  useEffect(() => {
+    if (passedValue !== value) {
+      setValue(formatPhoneNumber(passedValue, value));
+    }
+  }, [passedValue]);
 
   return <Input value={value} onChange={handleInputChange} {...inputProps} />;
 }
