a99e2ee8a15cd936fd0a3e66418287bf98b52c19
cehsu <claire@sappira.com>
2019-05-13 11:08:40 -0700

    Restore enzyme tests for blur and keypress trigger

--- src/Dropdown/Dropdown.spec.js
+++ src/Dropdown/Dropdown.spec.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { act } from 'react-dom/test-utils';
+import { mount } from 'enzyme';
 import { renderWithTheme, fireEvent, wait, waitForDomChange } from '../../test/utils';
+import defaultTheme from '../theme';
 import Dropdown from './Dropdown';
 import Button from '../Button';
 
@@ -25,22 +26,19 @@ describe('<Dropdown />', () => {
   const renderDropdown = () => {
     const wrapper = document.createElement('div');
     wrapper.setAttribute('tabindex', 1);
-    let utils;
-    act(() => {
-      utils = renderWithTheme(
-        <Dropdown portalNode={wrapper} trigger={<Button>Trigger</Button>}>
-          <Dropdown.Header>Header</Dropdown.Header>
-          <Dropdown.Body>
-            <Dropdown.Item data-testid="item-one">One</Dropdown.Item>
-            <Dropdown.Item data-testid="item-two">Two</Dropdown.Item>
-          </Dropdown.Body>
-          <Dropdown.Footer>Footer</Dropdown.Footer>
-        </Dropdown>,
-        {
-          container: document.body.appendChild(wrapper),
-        }
-      );
-    });
+    const utils = renderWithTheme(
+      <Dropdown portalNode={wrapper} trigger={<Button>Trigger</Button>}>
+        <Dropdown.Header>Header</Dropdown.Header>
+        <Dropdown.Body>
+          <Dropdown.Item data-testid="item-one">One</Dropdown.Item>
+          <Dropdown.Item data-testid="item-two">Two</Dropdown.Item>
+        </Dropdown.Body>
+        <Dropdown.Footer>Footer</Dropdown.Footer>
+      </Dropdown>,
+      {
+        container: document.body.appendChild(wrapper),
+      }
+    );
     return {
       wrapper,
       ...utils,
@@ -56,12 +54,16 @@ describe('<Dropdown />', () => {
     wait(() => {
       expect(renderUtils.queryByText('Header')).not.toBeInTheDocument();
     });
+ 
+  const assertMountedDropdownOpen = wrapper => {
+    expect(wrapper).toMatchSnapshot('is open');
+    expect(wrapper.find('footer')).toHaveLength(1);
+  }
 
-  const openDropdown = async () => {
+  const openDropdown = async (eventCallback) => {
     const trigger = renderUtils.getByText('Trigger');
-    act(() => {
-      fireEvent.click(trigger, { stopPropagation: () => null });
-    });
+    const callback = eventCallback || clickTrigger;
+    callback(trigger);
     return assertDropdownOpen();
   };
 
@@ -73,40 +75,59 @@ describe('<Dropdown />', () => {
     expect(renderUtils.asFragment()).toMatchSnapshot();
   });
 
-  test('opens menu with focus when trigger is clicked', async () => {
-    await openDropdown();
-    expect(renderUtils.asFragment()).toMatchSnapshot();
-    expect(renderUtils.getByTestId('dropdown-menu') === document.activeElement).toBeTruthy();
+  const stopPropagation = jest.fn()
+  const preventDefault = jest.fn()
+  const clickTrigger = trigger => {
+    fireEvent.click(trigger, { stopPropagation, preventDefault });
+  };
+
+  const baseTrigger = { stopPropagation, preventDefault };
+  const triggerEvents = {
+    'click': { type: 'click', ...baseTrigger },
+    'enter': { type: 'keypress', which: '32', ...baseTrigger },
+    'space': { type: 'keypress', which: '13', ...baseTrigger },
+  };
+
+  const mountAndOpenDropdown = (triggerEvent = triggerEvents.click) => {
+    const wrapper = mount(<Dropdown theme={defaultTheme} trigger={<div>Trigger</div>}>
+          <Dropdown.Header>Test</Dropdown.Header>
+          <Dropdown.Body>Body</Dropdown.Body>
+          <Dropdown.Footer>Footer</Dropdown.Footer>
+    </Dropdown>);
+
+    // open on trigger
+    const trigger = (wrapper.find('[role="button"]'));
+    trigger.simulate(triggerEvent.type, triggerEvent);
+    return wrapper;
+  };
+
+  // use enzyme since can't trigger keypress
+  Object.keys(triggerEvents).forEach( async eventKey => {
+    test(`menu opens with focus via ${eventKey} trigger`, async () => {
+       const wrapper = await mountAndOpenDropdown();
+       assertMountedDropdownOpen(wrapper);
+    });
   });
 
   test('closes when escape is pressed', async () => {
     await openDropdown();
-    act(() => {
-      fireEvent.keyDown(document.body, { key: 'Escape' });
-    });
+    fireEvent.keyDown(document.body, { key: 'Escape' });
     await assertDropdownClosed();
   });
 
-  test('closes when menu loses focus', async () => {
-    // Swallowing an annoying warning with act that's okay to ignore: https://github.com/facebook/react/issues/14769
-    const ogError = console.error;
-    console.error = _ => _;
+  // use enzyme since can't trigger blur via dom
+  test('closes on blur event', async () => {
+    const wrapper = await mountAndOpenDropdown();
+    assertMountedDropdownOpen(wrapper);
 
-    await openDropdown();
+    // blur dropdown menu
+    const menu = wrapper.find('.laCjkz');
+    menu.simulate('blur', { type: 'blur', stopPropagation, preventDefault });
 
-    // Some issues with fireEvent.focus: https://github.com/kentcdodds/react-testing-library/issues/276#issuecomment-473392827
-    act(() => {
-      renderUtils.wrapper.focus();
-    });
-    act(() => {
-      renderUtils.getByTestId('dropdown-menu').blur();
-    });
-
-    //await waitForDomChange();
-    await assertDropdownClosed();
-
-    console.error = ogError;
-  });
+    wrapper.update();
+    expect(wrapper).toMatchSnapshot('is closed');
+    expect(wrapper.find('footer')).toHaveLength(0);
+  })
 
   test('arrow keys navigate to focusable elements', async () => {
     await openDropdown();
@@ -115,20 +136,15 @@ describe('<Dropdown />', () => {
     const isFocused = node => node === document.activeElement;
 
     // First focusable element in tree should be selected on first arrow down
-    act(() => {
-      fireEvent.keyDown(document.body, { key: 'ArrowDown' });
-    });
+    fireEvent.keyDown(document.body, { key: 'ArrowDown' });
     expect(isFocused(itemOne)).toBeTruthy();
 
     // Arrow up on first item should bring us to last item
-    act(() => {
-      fireEvent.keyDown(document.body, { key: 'ArrowUp' });
-    });
+    fireEvent.keyDown(document.body, { key: 'ArrowUp' });
     expect(isFocused(itemTwo)).toBeTruthy();
 
-    act(() => {
-      fireEvent.keyDown(document.body, { key: 'ArrowUp' });
-    });
+    fireEvent.keyDown(document.body, { key: 'ArrowUp' });
     expect(isFocused(itemOne)).toBeTruthy();
   });
 });
+
