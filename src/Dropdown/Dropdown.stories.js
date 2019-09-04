cdc397c687cf91a5b472ddb21a370b1a83244ef4
Mathew Morris <hello@mathewmorris.com>
2019-09-04 11:08:20 -0500

    Minor style updates

--- src/Dropdown/Dropdown.stories.js
+++ src/Dropdown/Dropdown.stories.js
@@ -25,79 +25,89 @@ export const Basic = () => {
             }))}
             onChange={(_, val) => setPlacement(val)}
           />
-          <Flex mr={3}>
-            <Dropdown placement={placement} width={250} trigger={<Button variant="success">Open Dropdown</Button>}>
-              <Dropdown.Header title="Dropdown" />
+          <Dropdown
+            placement={placement}
+            width={250}
+            trigger={
+              <Button mr={3} variant="success">
+                Open Dropdown
+              </Button>
+            }>
+            <Dropdown.Header title="Dropdown" />
 
-              <Dropdown.Body>
+            <Dropdown.Body>
+              <Dropdown.Section>
                 <Dropdown.SectionTitle>Section One</Dropdown.SectionTitle>
                 <Dropdown.Item closeOnClick={false}>I don't close when clicked</Dropdown.Item>
                 <Dropdown.Item as="button">Item Two</Dropdown.Item>
+              </Dropdown.Section>
 
+              <Dropdown.Section>
                 <Dropdown.SectionTitle>Section Two</Dropdown.SectionTitle>
-                <Dropdown.Item as="a" closeOnClick href="http://google.com" target="_blank">
-                  Item One
-                </Dropdown.Item>
+                <Dropdown.Item as="a" onClick={close} href="http://google.com" target="_blank">Item One</Dropdown.Item>
                 <Dropdown.Item disabled>Item Two</Dropdown.Item>
-              </Dropdown.Body>
+              </Dropdown.Section>
+            </Dropdown.Body>
 
-              <Dropdown.Footer>Footer</Dropdown.Footer>
-            </Dropdown>
-          </Flex>
+            <Dropdown.Footer>Footer</Dropdown.Footer>
+          </Dropdown>
 
-          <Flex mr={3}>
-            <Dropdown width={250} trigger={<Button>Open Other Dropdown</Button>}>
-              <Dropdown.Header title="Dropdown" />
+          <Dropdown width={250} trigger={<Button mr={3}>Open Other Dropdown</Button>}>
+            <Dropdown.Header title="Dropdown" />
 
-              <Dropdown.Body>
+            <Dropdown.Body>
+              <Dropdown.Section>
                 <Dropdown.SectionTitle>Section One</Dropdown.SectionTitle>
                 <Dropdown.Item>Item One</Dropdown.Item>
                 <Dropdown.Item>Item Two</Dropdown.Item>
+              </Dropdown.Section>
 
+              <Dropdown.Section>
                 <Dropdown.SectionTitle>Section Two</Dropdown.SectionTitle>
-                <Dropdown.Item>Item One</Dropdown.Item>
+                <Dropdown.Item icon="alert-circle">Item One</Dropdown.Item>
                 <Dropdown.Item disabled>Item Two</Dropdown.Item>
-              </Dropdown.Body>
+              </Dropdown.Section>
+            </Dropdown.Body>
 
-              <Dropdown.Footer>Footer</Dropdown.Footer>
-            </Dropdown>
-          </Flex>
-
+            <Dropdown.Footer>Footer</Dropdown.Footer>
+          </Dropdown>
           <Flex mr={3}>
             <Dropdown width={250} placement="top" trigger={<Icon name="information-outline" />}>
               <Dropdown.Header title="Dropdown" />
 
               <Dropdown.Body>
-                <Dropdown.SectionTitle>Section One</Dropdown.SectionTitle>
-                <Dropdown.Item>Item One</Dropdown.Item>
-                <Dropdown.Item>Item Two</Dropdown.Item>
+                <Dropdown.Section>
+                  <Dropdown.SectionTitle>Section One</Dropdown.SectionTitle>
+                  <Dropdown.Item>Item One</Dropdown.Item>
+                  <Dropdown.Item>Item Two</Dropdown.Item>
+                </Dropdown.Section>
               </Dropdown.Body>
 
               <Dropdown.Footer>Footer</Dropdown.Footer>
             </Dropdown>
           </Flex>
 
-          <Flex flex={1}>
-            <Dropdown
-              width={250}
-              placement="top"
-              styles={{ Trigger: { display: 'flex', flex: 1 } }}
-              trigger={
-                <Flex flex={1} justifyContent="space-between">
-                  <Button style={{ width: `100%` }}>Open Flex Dropdown</Button>
-                </Flex>
-              }>
-              <Dropdown.Header title="Dropdown" />
+          <Dropdown
+            width={250}
+            placement="top"
+            styles={{ Trigger: { display: 'flex', flex: 1 } }}
+            trigger={
+              <Flex mr={3} flex={1} justifyContent="space-between">
+                <Button style={{ width: `100%` }}>Open Flex Dropdown</Button>
+              </Flex>
+            }>
+            <Dropdown.Header title="Dropdown" />
 
-              <Dropdown.Body>
+            <Dropdown.Body>
+              <Dropdown.Section>
                 <Dropdown.SectionTitle>Section One</Dropdown.SectionTitle>
                 <Dropdown.Item>Item One</Dropdown.Item>
                 <Dropdown.Item>Item Two</Dropdown.Item>
-              </Dropdown.Body>
+              </Dropdown.Section>
+            </Dropdown.Body>
 
-              <Dropdown.Footer>Footer</Dropdown.Footer>
-            </Dropdown>
-          </Flex>
+            <Dropdown.Footer>Footer</Dropdown.Footer>
+          </Dropdown>
         </Flex>
       </>
     );
