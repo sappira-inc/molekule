7a184456b120105f0a32594ad234e8018f6c1f75
Mathew Morris <hello@mathewmorris.com>
2020-01-24 19:09:35 -0600

    Upates Checkbox story

--- src/Form/Checkbox.stories.js
+++ src/Form/Checkbox.stories.js
@@ -8,9 +8,9 @@ export default {
   component: Checkbox,
 };
 
-export const Basic = () => (
-  <Checkbox id="checkbox" name="checkbox" label="I'm a checkbox" disabled={boolean('Disabled', false)} />
-);
+export const Basic = () => <Checkbox />;
+
+export const Label = () => <Checkbox label="I'm a checkbox" disabled={boolean('Disabled', false)} />;
 
 export const LongText = () => (
   <Flex width={300}>
